93d5aa42483f6797d27ee55e2e242067100b8a5b
SKhrebtan <shrebtan@gmail.com>
2024-02-07 15:15:37 +0200

    123456

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 import NextAuth from "next-auth";
 import authOptions from "../authOptions";
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST}
